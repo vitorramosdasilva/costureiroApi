ae8b9b20e4091f714876e16480e47e24c8a65a81
vitorramosdasilva <rsvitor_81@hotmail.com>
2021-03-28 19:07:11 -0300

    jwt_token2

--- my_project/settings.py
+++ my_project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'ABCX')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # INTERNAL_IPS = ['127.0.0.1']
@@ -152,7 +152,7 @@ AUTHENTICATION_BACKENDS = [
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
